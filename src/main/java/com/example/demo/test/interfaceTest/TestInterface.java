87eebee293a480b4967d54f89e94b82317de19e6
EverythingStudy <759499199@qq.com>
2019-12-04 18:34:03 +0800

    12-04 company-interface+abstract

---
+++ src/main/java/com/example/demo/test/interfaceTest/TestInterface.java
@@ -0,0 +1,20 @@
+package com.example.demo.test.interfaceTest;
+/**
+ * @Author cly
+ * @Description //TODO 接口
+ * 1.接口是特殊的抽象类
+ * 2.接口方法都是抽象方法没有具体实现，编译时自动为接口里定义的方法添加public abstract修饰符
+ * 3.实现类都必须实现所有方法
+ * 4.实现类可以实现多个接口
+ * 5.接口中的方法都是公有的
+ * 6.接口中可以定义静态final的变量，实现类中Class.price使用
+ * 接口保证了程序的解耦，安全
+ * @Date 17:49 2019/12/4
+ * @Param 
+ * @return 
+ **/
+
+public interface TestInterface {
+    public final  static int price=13;
+    public void getInterface();
+}

2ab2107f02f7afac52604d834d80872cdacb21f0
EverythingStudy <759499199@qq.com>
2020-02-07 16:53:24 +0800

    02-7 自定义拒绝策略

--- src/main/java/com/example/demo/test/Thread/TestThreadPoolExcutor.java
+++ src/main/java/com/example/demo/test/Thread/TestThreadPoolExcutor.java
@@ -1,28 +1,61 @@
 package com.example.demo.test.Thread;
 
 import org.apache.tomcat.util.threads.ThreadPoolExecutor;
+import org.junit.Test;
 
 import java.util.concurrent.*;
 
 /**
- * @Description java自身线程池
+ * @Description java自身线程池，自定义拒绝策略
  * @Date 2019/8/28 10:53
  * @Author cly
  **/
 public class TestThreadPoolExcutor {
-//默认结束策略
-    ExecutorService threadPoolExecutor=new ThreadPoolExecutor(2,10,0L, TimeUnit.SECONDS,new LinkedBlockingQueue<>(10));
-   public void test(){
+    ExecutorService threadPoolExecutor = new ThreadPoolExecutor(2, 2, 0L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(1), new RejectedExecutionHandler() {
+        @Override
+        public void rejectedExecution(Runnable r, java.util.concurrent.ThreadPoolExecutor executor) {
+            System.out.println("自定义拒绝策略");
+            throw new RejectedExecutionException("自定义拒绝策略");
+        }
+    });
 
-      FutureTask<String> futureTask= (FutureTask<String>) threadPoolExecutor.submit(new Callable<String>(){
-          @Override
-          public String call() throws Exception {
-              return null;
-          }
-      });
-   }
+    @Test
+    public void testCallable() {
+        for (int i=0;i<7;i++){
+            FutureTask<String> futureTask = (FutureTask<String>) threadPoolExecutor.submit(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    //待处理业务逻辑
+                    System.out.println("待处理业务逻辑");
+                    //Thread.sleep(1000);
+                    return "返回值";
+                }
+            });
+//            try {
+//                System.out.println(futureTask.get());
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            } catch (ExecutionException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
 
-   public void testNewFixedThreadPool(){
-       ExecutorService executorService=Executors.newFixedThreadPool(6);
-   }
+    /**
+     * @return void
+     * @Author cly
+     * @Description //TODO 创建固定个数线程池
+     * @Date 13:45 2020/2/7
+     * @Param []
+     **/
+    @Test
+    public void testNewFixedThreadPool() {
+        ExecutorService executorService = Executors.newFixedThreadPool(6);
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("==");
+            }
+        });
+    }
 }

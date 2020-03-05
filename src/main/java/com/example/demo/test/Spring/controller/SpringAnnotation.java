8396f4b33a736d3a68c34153d4108ded4f83e7e1
EverythingStudy <759499199@qq.com>
2020-03-05 11:53:44 +0800

    annotatoin

---
+++ src/main/java/com/example/demo/test/Spring/controller/SpringAnnotation.java
@@ -0,0 +1,36 @@
+package com.example.demo.test.Spring.controller;
+
+import com.example.demo.test.Base.TestList;
+import com.example.demo.test.Spring.entity.AdapterApi;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Description
+ * @Date 2020/2/20 11:56
+ * @Author cly
+ **/
+@RestController
+public class SpringAnnotation {
+    @RequestMapping
+    public void getPojo(@RequestBody AdapterApi adapterApi){
+
+    }
+    @RequestMapping("testttt")
+    public List<Map<String,Object>> TEST(){
+        TestList testList=new TestList();
+        Map<String,Object> map=new HashMap<>();
+        map.put("test","chen");
+        List<Map<String,Object>> list=new ArrayList<>();
+        list.add(map);
+        testList.setList(list);
+        return list;
+    }
+
+}

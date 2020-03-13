e105a664803887a8a44b27f07d67ffd8d89555f0
EverythingStudy <759499199@qq.com>
2020-03-13 15:00:37 +0800

    concurrentHashMap

--- src/main/java/com/example/demo/test/Map/TestHashMap.java
+++ src/main/java/com/example/demo/test/Map/TestHashMap.java
@@ -15,10 +15,11 @@ public class TestHashMap {
     Map<String,String> hashMap=new HashMap<>();
     /**
      * @Author cly
-     * @Description //TODO HashMap迭代器 除了迭代器中的next() 返回的是各种迭代器的值，hashNext()、nextNode()、remove()等方法都是继承于HashMap实现类的HashIterator内部类
-     *                //HashIterator内部类中定义了下面几个属性
-     *                Node<K,V> next;        // next entry to return
-     *                Node<K,V> current;     // current entry
+     * @Description //TODO HashMap迭代器 除了迭代器中的next() 返回的是各种迭代器的值，
+     * //TODO hashNext()、nextNode()、remove()等方法都是继承于HashMap实现类的HashIterator内部类
+     * //HashIterator内部类中定义了下面几个属性
+     *  Node<K,V> next;        // next entry to return
+     *  Node<K,V> concurrent;     // concurrent entry
      *                int expectedModCount;  // for fast-fail
      *                int index;当前元素的迭代位置
      * @Date 13:57 2019/11/27
@@ -37,7 +38,7 @@ public class TestHashMap {
 
     public static void main(String[] args) {
         Map map=new LinkedHashMap();
-        ConcurrentHashMap concurrentHashMap=new ConcurrentHashMap();
+        Map concurrentHashMap=new ConcurrentHashMap();
         boolean flag=true;
         int a=0;
         for(;a<10;){

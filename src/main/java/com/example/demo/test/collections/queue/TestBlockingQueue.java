d22c0f6657a8b4256341346c4a9a9e2b3bc89fff
EverythingStudy <759499199@qq.com>
2020-03-24 08:29:32 +0800

    BlockingQueue

--- src/main/java/com/example/demo/test/collections/queue/TestBlockingQueue.java
+++ src/main/java/com/example/demo/test/collections/queue/TestBlockingQueue.java
@@ -1,9 +1,8 @@
-package com.example.demo.test.queue;
+package com.example.demo.test.collections.queue;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.PriorityBlockingQueue;
 
 /**
  * @Description

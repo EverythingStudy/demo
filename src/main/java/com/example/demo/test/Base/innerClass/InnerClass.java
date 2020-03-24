39ec433f2afd95aeb331749b6b7d5f2f7f7c3c61
chenly <759499199@qq.com>
2020-03-24 08:51:32 +0800

    innerClass

--- src/main/java/com/example/demo/test/Base/innerClass/InnerClass.java
+++ src/main/java/com/example/demo/test/Base/innerClass/InnerClass.java
@@ -9,22 +9,27 @@ public class InnerClass {
         //外部类使用内部类的方法只能通过实例化内部类来调用，不能直接使用
         InnerClass.InnerClassTest innerClassTest=new InnerClass().new InnerClassTest();
         innerClassTest.getAge();
+        System.out.println(innerClassTest.age);
         System.out.println(name);
     }
     class InnerClassTest{
-        public String age;
+        public String age="13Y";
         public void getAge(){
             //使用外部类的方法
-            getName("内部类调用");
+           //getName("内部类调用");
             System.out.println("内部类");
         }
     }
 
+    /**
+     * 出现了循环引用--注释部分都打开的话
+     * @param args
+     */
     public static void main(String[] args) {
         InnerClass innerClass=new InnerClass();
         innerClass.getName("外部类调用");;
-        InnerClass.InnerClassTest innerClassTest=new InnerClass().new InnerClassTest();
-        innerClassTest.getAge();
+       // InnerClass.InnerClassTest innerClassTest=new InnerClass().new InnerClassTest();
+        //innerClassTest.getAge();
 
     }
 }

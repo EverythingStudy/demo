62287be28a4794dd8fa0f5c82c2cc0bd9d285d92
EverythingStudy <759499199@qq.com>
2020-03-24 08:41:17 +0800

    @ApiIgnore

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,16 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement// 开启注解事务管理，等同于xml配置文件中的 <tx:annotation-driven />
 @SpringBootApplication
-@MapperScan("com.example.demo.test.Spring.dao")//作用
+//扫描mapper.xml文件
+@MapperScan("com.example.demo.test.Spring.dao")
+//ehcache 开启
+@EnableCaching
 public class DemoApplication {
 
     public static void main(String[] args) {

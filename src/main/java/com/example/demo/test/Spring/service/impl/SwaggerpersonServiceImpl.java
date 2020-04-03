1c35eef02620f4813b3242142b05051744be5598
EverythingStudy <759499199@qq.com>
2020-04-03 15:36:45 +0800

    线程执行类

--- src/main/java/com/example/demo/test/Spring/service/impl/SwaggerpersonServiceImpl.java
+++ src/main/java/com/example/demo/test/Spring/service/impl/SwaggerpersonServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.demo.test.Spring.service.impl;
 import com.example.demo.test.Spring.entity.Swaggerperson;
 import com.example.demo.test.Spring.dao.SwaggerpersonDao;
 import com.example.demo.test.Spring.service.SwaggerpersonService;
+import com.example.demo.test.concurrent.Thread.ThreadPoolBySlef.MyFutureTask;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * (Swaggerperson)表服务实现类
@@ -18,6 +20,7 @@ import java.util.List;
 public class SwaggerpersonServiceImpl implements SwaggerpersonService {
     @Resource
     private SwaggerpersonDao swaggerpersonDao;
+
 
     /**
      * 通过ID查询单条数据
@@ -34,7 +37,7 @@ public class SwaggerpersonServiceImpl implements SwaggerpersonService {
      * 查询多条数据
      *
      * @param offset 查询起始位置
-     * @param limit 查询条数
+     * @param limit  查询条数
      * @return 对象列表
      */
     @Override
@@ -48,10 +51,70 @@ public class SwaggerpersonServiceImpl implements SwaggerpersonService {
      * @param swaggerperson 实例对象
      * @return 实例对象
      */
+
     @Override
     public Swaggerperson insert(Swaggerperson swaggerperson) {
-        this.swaggerpersonDao.insert(swaggerperson);
-        return swaggerperson;
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int count = swaggerpersonDao.insert(swaggerperson);
+                System.out.println(count);
+                try {
+                    Thread.sleep(3 * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        executorService.submit(thread);
+        //定义带返回值的线程
+        Callable callable = new Thread2();
+        MyFutureTask myFutureTask=new MyFutureTask(callable);
+        new Thread(myFutureTask).start();
+        //定义带返回值的线程
+        Callable callable3 = new Thread3();
+        MyFutureTask myFutureTask3=new MyFutureTask(callable3);
+        new Thread(myFutureTask3).start();
+        Swaggerperson swaggerpersonT = null;
+        try {
+            swaggerpersonT =(Swaggerperson)myFutureTask.get();
+            System.out.println("2=="+swaggerpersonT.toString());
+            Swaggerperson swaggerpersonk=(Swaggerperson) myFutureTask3.get();
+            System.out.println("3==="+swaggerpersonk.toString());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return swaggerpersonT;
+    }
+
+    class Thread2 implements Callable {
+        @Override
+        public Swaggerperson call() {
+            try {
+                Thread.sleep(1 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Swaggerperson swaggerpersons = swaggerpersonDao.queryById(1);
+            return swaggerpersons;
+
+        }
+    }
+    class Thread3 implements Callable {
+        @Override
+        public Swaggerperson call() {
+            try {
+                Thread.sleep(5 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Swaggerperson swaggerpersons = swaggerpersonDao.queryById(18);
+            return swaggerpersons;
+
+        }
     }
 
     /**

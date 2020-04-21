ef95c76c14c6734ba0a29c3c00d6c80bf73e3b76
EverythingStudy <759499199@qq.com>
2020-04-21 17:19:40 +0800

    Semaphore

--- src/main/java/com/example/demo/test/concurrent/TestSemaphore.java
+++ src/main/java/com/example/demo/test/concurrent/TestSemaphore.java
@@ -1,6 +1,9 @@
 package com.example.demo.test.concurrent;
 
+import org.junit.Test;
+
 import java.util.concurrent.Semaphore;
+
 
 /**
  * @Description 信号量--Semaphore
@@ -8,5 +11,28 @@ import java.util.concurrent.Semaphore;
  * @Author cly
  **/
 public class TestSemaphore extends Object{
-    Semaphore semaphore=new Semaphore(10);
+    Semaphore semaphore=new Semaphore(2);
+
+    public void test(){
+        for (int i=0;i<10;i++){
+            new Thread(()->{
+                try {
+                    //分配一个信号
+                    semaphore.acquire();
+                    System.out.println("=1=="+Thread.currentThread().getName());
+                    Thread.sleep(1000*2);
+                    System.out.println("=2=="+Thread.currentThread().getName());
+                    //回收一个信号
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+
+    public static void main(String[] args) {
+        TestSemaphore testSemaphore=new TestSemaphore();
+        testSemaphore.test();
+    }
 }
